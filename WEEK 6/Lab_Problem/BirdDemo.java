9b6c798e4e7385a8d204fb583ceeaef762ccefb5
Hanishka Kela <hanishkaappl@gmail.com>
2025-09-20 17:40:35 +0530

    Method Overriding with @Override

---
+++ WEEK 6/Lab_Problem/BirdDemo.java
@@ -0,0 +1,30 @@
+package Lab_Problem;
+
+public class BirdDemo {
+    public static void main(String[] args) {
+        Bird[] birds = { new Penguin(), new Eagle() };
+        for (Bird b : birds) {
+            b.fly();
+        }
+    }
+}
+
+class Bird {
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+}
+
+class Penguin extends Bird {
+    @Override
+    public void fly() {
+        System.out.println("Penguin cannot fly, it swims");
+    }
+}
+
+class Eagle extends Bird {
+    @Override
+    public void fly() {
+        System.out.println("Eagle soars high");
+    }
+}
