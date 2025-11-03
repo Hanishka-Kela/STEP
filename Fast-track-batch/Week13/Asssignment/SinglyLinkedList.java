17b1210242c2a8da5c6b2de95ea1d1b9f637488e
Hanishka-Kela <hanishkaappl@gmail.com>
2025-11-03 14:49:26 +0530

    Insertion at a given position

---
+++ Fast-track-batch/Week13/Asssignment/SinglyLinkedList.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class SinglyLinkedList {
+    Node head;
+    void insertAtPosition(int data, int position) {
+        Node newNode = new Node(data);
+        if (position == 1) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < position - 1 && temp != null; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Invalid position");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data);
+            if (temp.next != null) System.out.print(" → ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.head = new Node(10);
+        list.head.next = new Node(20);
+        list.head.next.next = new Node(30);
+        list.head.next.next.next = new Node(40);
+        System.out.print("Before: ");
+        list.display();
+        list.insertAtPosition(50, 3);
+        System.out.print("After: ");
+        list.display();
+    }
+}
